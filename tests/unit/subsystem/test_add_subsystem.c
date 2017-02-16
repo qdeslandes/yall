06ed48e64a1c25c2f351b65512f55d2e93a9bddd
qdeslandes <quentin.deslandes@hrv-simulation.com>
2017-02-16 15:27:31 +0100

    Add tests for add_subsystem()

---
+++ tests/unit/subsystem/test_add_subsystem.c
@@ -0,0 +1,47 @@
+#include <criterion/criterion.h>
+#include "yall/subsystem.h"
+#include "h_subsystem.h"
+
+extern struct yall_subsystem *subsystems;
+extern struct yall_subsystem *_subsystems[10];
+
+Test(subsystem, test_add_subsystem0)
+{
+    // Without parent
+	struct yall_subsystem *s = get_fake_subsystem("0", NULL);
+    add_subsystem(NULL, s);
+    cr_assert_eq(subsystems, s);
+
+    s = get_fake_subsystem("1", NULL);
+    add_subsystem(NULL, s);
+    cr_assert_eq(subsystems->next, s);
+    cr_assert_eq(s->previous, subsystems);
+
+    s = get_fake_subsystem("2", NULL);
+    add_subsystem(NULL, s);
+    cr_assert_eq(subsystems->next->next, s);
+    cr_assert_eq(s->previous, subsystems->next);
+
+    // With parent
+    s = get_fake_subsystem("00", NULL);
+    add_subsystem("0", s);
+    cr_assert_eq(subsystems->childs, s);
+    cr_assert_eq(s->parent, subsystems);
+
+    s = get_fake_subsystem("20", NULL);
+    add_subsystem("2", s);
+    cr_assert_eq(subsystems->next->next->childs, s);
+    cr_assert_eq(s->parent, subsystems->next->next);
+
+    s = get_fake_subsystem("21", NULL);
+    add_subsystem("2", s);
+    cr_assert_eq(subsystems->next->next->childs->next, s);
+    cr_assert_eq(s->parent, subsystems->next->next);
+    cr_assert_eq(s->previous, subsystems->next->next->childs);
+
+    // Without parent (last time)
+    s = get_fake_subsystem("3", NULL);
+    add_subsystem(NULL, s);
+    cr_assert_eq(subsystems->next->next->next, s);
+    cr_assert_eq(s->previous, subsystems->next->next);
+}
