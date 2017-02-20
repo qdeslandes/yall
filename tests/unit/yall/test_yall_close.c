5093bb557a0ea384ddbac8373b04678e7830a86d
Naccyde <naccyde@naccyde.eu>
2017-02-20 10:21:13 +0100

    Test for yall_init() and yall_close()

---
+++ tests/unit/yall/test_yall_close.c
@@ -0,0 +1,20 @@
+#include <criterion/criterion.h>
+#include <stdbool.h>
+#include "yall/yall.h"
+#include "yall/errors.h"
+#include "h_stream.h"
+
+extern bool initialized;
+
+Test(subsystem, test_yall_close0)
+{
+	cr_assert_eq(yall_close(), YALL_NOT_INIT);
+
+	yall_init();
+	cr_assert_eq(yall_close(), YALL_OK);
+	cr_assert_eq(yall_close(), YALL_NOT_INIT);
+
+	yall_init();
+	cr_assert_eq(yall_close(), YALL_OK);
+	cr_assert_eq(yall_close(), YALL_NOT_INIT);
+}
