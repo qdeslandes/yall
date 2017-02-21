78739b4a0a7eae53ac1bbb68d48442474c158a6f
Naccyde <naccyde@naccyde.eu>
2017-02-21 14:24:49 +0100

    Add real test directory

---
+++ tests/real/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include <yall/yall.h>
+
+int main(void)
+{
+	yall_init();
+
+	yall_set_subsystem("test", NULL, yall_debug, yall_console_output, NULL);
+	yall_set_subsystem("longsubsystem", NULL, yall_debug, yall_console_output, NULL);
+	yall_set_subsystem("azertyazertyazerty", NULL, yall_debug, yall_console_output, NULL);
+
+	YALL_DEBUG("test", "This is a test log message");
+	YALL_WARNING("longsubsystem", "This is a test log message");
+	YALL_ERR("test", "This is a test log message");
+	YALL_ERR("azertyazertyazerty", "test");
+
+	return 0;
+}
