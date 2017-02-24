2c4562c4063b7c757c3162507ac8bacc7b46128c
Naccyde <naccyde@naccyde.eu>
2017-02-24 11:47:50 +0100

    Test failure cases log write

--- tests/unit/console/test_write_log_console.c
+++ tests/unit/console/test_write_log_console.c
@@ -2,6 +2,7 @@
 #include <criterion/parameterized.h>
 #include <criterion/redirect.h>
 
+#include "h_utils.h"
 #include "test_console.h"
 #include "yall/console.h"
 #include "yall/errors.h"
@@ -32,6 +33,9 @@ void test_write_log_console_clean(void)
 #endif
 }
 
+/*
+ * Empty sentence to display
+ */
 ParameterizedTestParameters(console, test_write_log_console0) {
     return cr_make_param_array(struct param_set_color, ll_and_colors, 8);
 }
@@ -51,6 +55,9 @@ ParameterizedTest(
     cr_assert_stderr_eq_str(output);
 }
 
+/*
+ * Non empty sentence to display
+ */
 ParameterizedTestParameters(console, test_write_log_console1) {
     return cr_make_param_array(struct param_set_color, ll_and_colors, 8);
 }
@@ -68,4 +75,30 @@ ParameterizedTest(
     char output[18] = { 0 };
     sprintf(output, "\033[%dmsentence\033[0m", p->code);
     cr_assert_stderr_eq_str(output);
+}
+
+/*
+ * Failing sem_wait
+ */
+Test(console,
+    test_write_log_console2,
+    .init=test_write_log_console_setup,
+    .fini=test_write_log_console_clean)
+{
+    DISABLE_SEM_WAIT();
+    cr_assert_eq(write_log_console(yall_debug, "nope"), YALL_CONSOLE_LOCK_ERR);
+    ENABLE_SEM_WAIT();
+}
+
+/*
+ * Failing fprintf
+ */
+Test(console,
+    test_write_log_console3,
+    .init=test_write_log_console_setup,
+    .fini=test_write_log_console_clean)
+{
+    DISABLE_FPRINTF();
+    cr_assert_eq(write_log_console(yall_debug, "nope"), YALL_CONSOLE_WRITE_ERR);
+    ENABLE_FPRINTF();
 }
