d78b134337bb6d30465e2991fe884c95bf70bb1f
Naccyde <naccyde@naccyde.eu>
2017-03-01 10:54:31 +0100

    Fix generate_header() tests

--- tests/unit/message/test_generate_header.c
+++ tests/unit/message/test_generate_header.c
@@ -1,15 +1,15 @@
 #include "test_message.h"
 
 struct param_test_generate_header message_params[9] = {
-	{ "", yall_debug, "", "				 ::: DEBUG	 ::			  :: " },
-	{ "", yall_warning, "", "				 ::: WARNING   ::			  :: " },
-	{ "", yall_emerg, "", "				 ::: EMERGENCY ::			  :: " },
-	{ "test", yall_debug, "", "test			 ::: DEBUG	 ::			  :: " },
-	{ "test", yall_emerg, "", "test			 ::: EMERGENCY ::			  :: " },
-	{ "", yall_debug, "test", "				 ::: DEBUG	 :: test		 :: " },
-	{ "", yall_emerg, "test", "				 ::: EMERGENCY :: test		 :: " },
-	{ "test", yall_debug, "test", "test			 ::: DEBUG	 :: test		 :: " },
-	{ "test", yall_emerg, "test", "test			 ::: EMERGENCY :: test		 :: " }
+	{ "", yall_debug, "", "                 ::: DEBUG     ::              :: " },
+	{ "", yall_warning, "", "                 ::: WARNING   ::              :: " },
+	{ "", yall_emerg, "", "                 ::: EMERGENCY ::              :: " },
+	{ "test", yall_debug, "", "test             ::: DEBUG     ::              :: " },
+	{ "test", yall_emerg, "", "test             ::: EMERGENCY ::              :: " },
+	{ "", yall_debug, "test", "                 ::: DEBUG     :: test         :: " },
+	{ "", yall_emerg, "test", "                 ::: EMERGENCY :: test         :: " },
+	{ "test", yall_debug, "test", "test             ::: DEBUG     :: test         :: " },
+	{ "test", yall_emerg, "test", "test             ::: EMERGENCY :: test         :: " }
 };
 
 /*
