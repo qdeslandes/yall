a9379bfa5e147feca31cf90001baae85b243e67c
Naccyde <naccyde@naccyde.eu>
2017-04-13 17:36:45 +0200

    Update yall* tests

--- tests/unit/yall/test_yall_call_log.c
+++ tests/unit/yall/test_yall_call_log.c
@@ -1,6 +1,7 @@
 #include "test_yall.h"
 
 extern struct yall_subsystem *subsystems;
+extern struct yall_subsystem *_subsystems[10];
 
 /*
  * Library not initialized
@@ -13,25 +14,15 @@ Test(yall, test_yall_call_log0)
 }
 
 /*
- * Empty subsystems list / failing yall_log
- */
-Test(yall, test_yall_call_log1, .init=tests_yall_init_lib, .fini=tests_yall_close_lib)
-{
-	cr_assert_eq(yall_call_log("", yall_debug, "", tests_call_log_function, NULL), YALL_LOG_LEVEL_TOO_LOW);
-	cr_assert_eq(yall_call_log("nope", yall_warning, "", tests_call_log_function, NULL), YALL_OK);
-	cr_assert_eq(yall_call_log("toolongnameforasubsysteminthelibrary", yall_err, "", tests_call_log_function, NULL), YALL_OK);
-}
-
-/*
  * Test on getting subsystem and checking log level
  */
-TheoryDataPoints(yall, test_yall_call_log2) = {
+TheoryDataPoints(yall, test_yall_call_log1) = {
 	DataPoints(char *, "0", "00", "01", "02", "1", "2", "20", "200", "201", "3"),
 	DataPoints(enum yall_log_level, yall_debug, yall_info, yall_notice, yall_warning, yall_err, yall_crit, yall_alert, yall_emerg),
 	DataPoints(char *, "toolongnameforafunctionnameinthelibrary", "main", "int main()", "main()", "Class::Method", "int Class::Method", "int Class::Method()"),
 };
 
-Theory((char *s, enum yall_log_level ll, char *f), yall, test_yall_call_log2, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
+Theory((char *s, enum yall_log_level ll, char *f), yall, test_yall_call_log1, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
 {
 	uint8_t waiting_for = YALL_OK;
 	uint8_t ret = yall_call_log(s, ll, f, tests_call_log_function, NULL);
@@ -47,7 +38,7 @@ Theory((char *s, enum yall_log_level ll, char *f), yall, test_yall_call_log2, .i
 /*
  * Calling the logging function with parameters
  */
-Test(yall, test_yall_call_log3, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
+Test(yall, test_yall_call_log2, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
 {
 	char buff[40] = { 0 };
 
@@ -59,9 +50,19 @@ Test(yall, test_yall_call_log3, .init=tests_yall_log_setup, .fini=tests_yall_log
 /*
  * Failed in writing message
  */
-Test(yall, test_yall_call_log4, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
+Test(yall, test_yall_call_log3, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
 {
 	disable_fprintf();
 	cr_assert_eq(yall_call_log("01", yall_emerg, "", tests_call_log_function, NULL), YALL_CONSOLE_WRITE_ERR);
 	enable_fprintf();
+}
+
+/*
+ * Disabled subsystem
+ */
+Test(yall, test_yall_call_log4, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
+{
+        _subsystems[0]->status = yall_subsys_disable;
+
+        cr_assert_eq(yall_call_log("0", yall_emerg, "", tests_call_log_function, NULL), YALL_SUBSYS_DISABLED);
 }
--- tests/unit/yall/test_yall_set_subsystem.c
+++ tests/unit/yall/test_yall_set_subsystem.c
@@ -3,9 +3,17 @@
 extern struct yall_subsystem *subsystems;
 
 /*
+ * Library not initialized
+ */
+Test(yall, test_yall_set_subsystem0)
+{
+	cr_assert_eq(yall_set_subsystem(NULL, NULL, yall_debug, yall_console_output, NULL), YALL_NOT_INIT);
+}
+
+/*
  * Bad name
  */
-Test(yall, test_yall_set_subsystem0, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
+Test(yall, test_yall_set_subsystem1, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
 {
 	cr_assert_eq(yall_set_subsystem(NULL, NULL, yall_debug, yall_console_output, NULL), YALL_NO_NAME);
 }
@@ -13,7 +21,7 @@ Test(yall, test_yall_set_subsystem0, .init=tests_yall_log_setup, .fini=tests_yal
 /*
  * Need to update an existing subsystem
  */
-Test(yall, test_yall_set_subsystem1, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
+Test(yall, test_yall_set_subsystem2, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
 {
 	cr_assert_eq(yall_set_subsystem("0", NULL, yall_debug, yall_console_output, NULL), YALL_OK);
 }
@@ -21,7 +29,7 @@ Test(yall, test_yall_set_subsystem1, .init=tests_yall_log_setup, .fini=tests_yal
 /*
  * Need to create a subsystem
  */
-Test(yall, test_yall_set_subsystem2, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
+Test(yall, test_yall_set_subsystem3, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
 {
 	cr_assert_eq(yall_set_subsystem("test", NULL, yall_debug, yall_console_output, NULL), YALL_OK);
 }
@@ -29,7 +37,7 @@ Test(yall, test_yall_set_subsystem2, .init=tests_yall_log_setup, .fini=tests_yal
 /*
  * create_subsystem() fail
  */
-Test(yall, test_yall_set_subsystem3, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
+Test(yall, test_yall_set_subsystem4, .init=tests_yall_log_setup, .fini=tests_yall_log_clean)
 {
 	disable_malloc();
 	cr_assert_eq(yall_set_subsystem("test", NULL, yall_debug, yall_console_output, NULL), YALL_CANT_CREATE_SUBSYS);
