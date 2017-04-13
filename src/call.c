d0805b17de5855468480906f4f47240030eefdcd
Naccyde <naccyde@naccyde.eu>
2017-04-13 17:29:45 +0200

    Add add_line function for lines list

--- src/call.c
+++ src/call.c
@@ -1,10 +1,28 @@
 #include "yall/call.h"
 
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 
-static void call_data_to_buffer(char *buffer, struct yall_call_data *d)
+/*
+ * add_line : add the given <content> to a new line of the data <d>.
+ *      <d> and <content> can't be NULL.
+ */
+static void add_line(struct yall_call_data *d, char *content)
 {
+        struct yall_call_data_line *l = malloc(sizeof(struct yall_call_data_line));
 
+        if (d->lines == NULL)
+                d->lines = l;
+        else {
+                struct yall_call_data_line *tmp = d->lines;
+                for ( ; tmp->next; tmp = tmp->next) ;
+                tmp->next = l;
+        }
+
+        l->content = content;
+        l->next = NULL;
 }
 
 const char *call_custom_formatter(
