c707dd0b01ebcc0dfed907fcb4e18a3e9eddefda
Naccyde <naccyde@naccyde.eu>
2017-04-13 17:37:24 +0200

    Create real conditions tests for YALL_CALL....

--- tests/real/main.c
+++ tests/real/main.c
@@ -2,6 +2,17 @@
 
 #include <yall/yall.h>
 
+void formatter(struct yall_call_data *d, void *args)
+{
+        int lines = 3;
+
+        yall_call_set_header(d, "Data report for today :");
+        yall_call_add_line(d, 0, "Total lines : %d", lines);
+        yall_call_add_line(d, 0, "Languages :");
+        yall_call_add_line(d, 1, "C : 0");
+        yall_call_add_line(d, 1, "C++ : 3");
+}
+
 int main(void)
 {
 	yall_init();
@@ -27,9 +38,15 @@ int main(void)
 	yall_enable_subsystem("status");
 	YALL_ERR("status", "This should appear");
 
+        YALL_CALL_DEBUG("test", formatter, NULL);
+
+        YALL_DEBUG("test", "lol");
+
 #ifdef _WIN32
 	getchar();
 #endif
+
+        yall_close();
 
 	return 0;
 }
