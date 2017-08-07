cc2f4c2458ddd2a2553dd2750511d1891eed6d0d
qdeslandes <quentin.deslandes@hrv-simulation.com>
2017-08-07 13:57:53 +0200

    Create queue mechanism

---
+++ src/queue.c
@@ -0,0 +1,72 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Quentin "Naccyde" Deslandes.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "yall/queue.h"
+
+#include <stdlib.h>
+
+#include <Windows.h>
+#include <malloc.h>
+
+static struct qnode *head = NULL;
+
+volatile struct qnode *qnode_new(void *data)
+{
+	volatile struct qnode *node = _aligned_malloc(sizeof(struct qnode), 64);
+
+	node->next = NULL;
+	node->data = data;
+
+	return node;
+}
+
+void qnode_delete(volatile struct qnode *node)
+{
+	_aligned_free((struct qnode *)node);
+}
+
+void enqueue(void *data)
+{
+	volatile struct qnode *new_node = qnode_new(data);
+	volatile struct qnode *orig_head = head;
+
+	do {
+		orig_head = head;
+		new_node->next = orig_head;
+	} while (orig_head != InterlockedCompareExchangePointer(&head, new_node, orig_head));
+}
+
+volatile struct qnode *swap_queue(void)
+{
+	if (! head)
+		return NULL;
+
+	volatile struct qnode *orig_head = NULL;
+
+	do {
+		orig_head = head;
+	} while (orig_head != InterlockedCompareExchangePointer(&head, NULL, orig_head));
+
+	return orig_head;
+}
