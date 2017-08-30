bec57da9794774e581171653b8436f05528c031d
Naccyde <naccyde@naccyde.eu>
2017-08-30 20:57:44 +0200

    Adapt real application to linux

--- tests/real/main.c
+++ tests/real/main.c
@@ -6,14 +6,20 @@
 
 #define N_THREADS	10
 
+#ifdef __linux__
+#define yall_sleep(ms) usleep(ms * 1000)
+static bool thread_run __attribute__((aligned(64))) = true;
+#elif _WIN32
 static __declspec(align(64)) bool thread_run = true;
+#define yall_sleep(ms) Sleep(ms)
+#endif
 
 void *t0(void *args)
 {
 	while (thread_run) {
 		YALL_INFO("t0", "Thread 0 message.");
 
-		Sleep(10);
+		yall_sleep(10);
 	}
 
 	return NULL;
@@ -24,7 +30,7 @@ void *t1(void *args)
 	while (thread_run) {
 		YALL_WARNING("t1", "Thread 1 message.");
 
-		Sleep(10);
+		yall_sleep(10);
 	}
 
 	return NULL;
@@ -35,7 +41,7 @@ void *t2(void *args)
 	while (thread_run) {
 		YALL_ERR("t2", "Thread 2 message.");
 
-		Sleep(10);
+		yall_sleep(10);
 	}
 
 	return NULL;
@@ -46,7 +52,7 @@ void *t3(void *args)
 	while (thread_run) {
 		YALL_ERR("t3", "Thread 3 message.");
 
-		Sleep(10);
+		yall_sleep(10);
 	}
 
 	return NULL;
@@ -57,7 +63,7 @@ void *t4(void *args)
 	while (thread_run) {
 		YALL_ERR("t4", "Thread 4 message.");
 
-		Sleep(10);
+		yall_sleep(10);
 	}
 
 	return NULL;
@@ -68,7 +74,7 @@ void *t5(void *args)
 	while (thread_run) {
 		YALL_ERR("t5", "Thread 5 message.");
 
-		Sleep(10);
+		yall_sleep(10);
 	}
 
 	return NULL;
@@ -79,7 +85,7 @@ void *t6(void *args)
 	while (thread_run) {
 		YALL_ERR("t6", "Thread 6 message.");
 
-		Sleep(10);
+		yall_sleep(10);
 	}
 
 	return NULL;
@@ -90,7 +96,7 @@ void *t7(void *args)
 	while (thread_run) {
 		YALL_ERR("t7", "Thread 7 message.");
 
-		Sleep(10);
+		yall_sleep(10);
 	}
 
 	return NULL;
@@ -101,7 +107,7 @@ void *t8(void *args)
 	while (thread_run) {
 		YALL_ERR("t8", "Thread 8 message.");
 
-		Sleep(10);
+		yall_sleep(10);
 	}
 
 	return NULL;
@@ -112,7 +118,7 @@ void *t9(void *args)
 	while (thread_run) {
 		YALL_ERR("t9", "Thread 9 message.");
 
-		Sleep(10);
+		yall_sleep(10);
 	}
 
 	return NULL;
@@ -127,16 +133,19 @@ int main(void)
 
 	yall_set_subsystem("YALLreal", NULL, yall_debug, yall_console_output, NULL);
 	yall_set_subsystem("setup", "YALLreal", yall_debug, yall_console_output, NULL);
-	yall_set_subsystem("t0", "YALLreal", yall_debug, yall_console_output, NULL);
-	yall_set_subsystem("t1", "YALLreal", yall_debug, yall_console_output, NULL);
-	yall_set_subsystem("t2", "YALLreal", yall_debug, yall_console_output, NULL);
-	yall_set_subsystem("t3", "YALLreal", yall_debug, yall_console_output, NULL);
-	yall_set_subsystem("t4", "YALLreal", yall_debug, yall_console_output, NULL);
-	yall_set_subsystem("t5", "YALLreal", yall_debug, yall_console_output, NULL);
-	yall_set_subsystem("t6", "YALLreal", yall_debug, yall_console_output, NULL);
-	yall_set_subsystem("t7", "YALLreal", yall_debug, yall_console_output, NULL);
-	yall_set_subsystem("t8", "YALLreal", yall_debug, yall_console_output, NULL);
-	yall_set_subsystem("t9", "YALLreal", yall_debug, yall_console_output, NULL);
+	yall_set_subsystem("threads", "YALLreal", yall_debug, yall_console_output, NULL);
+	yall_set_subsystem("t0", "threads", yall_debug, yall_console_output, NULL);
+	yall_set_subsystem("t1", "threads", yall_debug, yall_console_output, NULL);
+	yall_set_subsystem("t2", "threads", yall_debug, yall_console_output, NULL);
+	yall_set_subsystem("t3", "threads", yall_debug, yall_console_output, NULL);
+	yall_set_subsystem("t4", "threads", yall_debug, yall_console_output, NULL);
+	yall_set_subsystem("t5", "threads", yall_debug, yall_console_output, NULL);
+	yall_set_subsystem("t6", "threads", yall_debug, yall_console_output, NULL);
+	yall_set_subsystem("t7", "threads", yall_debug, yall_console_output, NULL);
+	yall_set_subsystem("t8", "threads", yall_debug, yall_console_output, NULL);
+	yall_set_subsystem("t9", "threads", yall_debug, yall_console_output, NULL);
+
+	yall_disable_subsystem("threads");
 
 	YALL_INFO("setup", "Starting threads.");
 
@@ -144,17 +153,18 @@ int main(void)
 		int ret = pthread_create(&threads[0], NULL, tfunc[i], NULL);
 		if (ret != 0)
 			YALL_ERR("setup", "Thread %d startup failed.", i);
+		else
+			YALL_INFO("setup", "Thread %d started.", i);
 	}
 
 	YALL_INFO("setup", "Threads running, hit a key to stop.");
 
-#ifdef _WIN32
 	getchar();
-#endif
 
 	YALL_INFO("setup", "Closing threads.");
 
 	thread_run = false;
+
 	for (int i = 0; i < N_THREADS; ++i)
 		pthread_join(threads[i], NULL);
 
@@ -162,9 +172,7 @@ int main(void)
 
 	printf("Threads stopped, hit a key to quit...\n");
 
-#ifdef _WIN32
 	getchar();
-#endif
 
 	return 0;
 }
