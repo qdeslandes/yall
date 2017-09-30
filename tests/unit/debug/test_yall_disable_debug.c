dea53d47743a47a9490b9122c56086ee8482cfd7
naccyde <naccyde@naccyde.eu>
2017-09-30 17:46:22 +0200

    Create debug.c with function to enable / disable debug mode

---
+++ tests/unit/debug/test_yall_disable_debug.c
@@ -0,0 +1,39 @@
+#include "test_debug.h"
+
+extern uint16_t initialized;
+extern bool debug;
+
+/*
+ * Disactivation
+ */
+Test(debug, test_yall_disable_debug0)
+{
+        yall_disable_debug();
+        cr_assert_eq(debug, false);
+
+        yall_init();
+        yall_enable_debug();
+        yall_disable_debug();
+        cr_assert_eq(debug, false);
+        yall_close_all();
+}
+
+/*
+ * Subsystem deletion
+ */
+Test(debug, test_yall_disable_debug1)
+{
+        yall_init();
+        yall_enable_debug();
+
+        #ifdef DEBUG
+        cr_assert_eq(debug, true);
+        #else
+        cr_assert_eq(debug, false);
+        #endif
+
+        yall_close_all();
+
+        struct yall_subsystem_params p = { 0 };
+        cr_assert_eq(get_subsystem("yall", &p), NULL);
+}
