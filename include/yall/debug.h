05131c40c9c024977d62e4e85dc72d9516a02cb3
naccyde <quentin.deslandes@hrv-simulation.com>
2017-10-02 12:00:42 +0200

    Fix sonar '#' warnings

--- include/yall/debug.h
+++ include/yall/debug.h
@@ -27,21 +27,21 @@
 
 
 #ifdef YALL_UNIT
-        #include <stdio.h>
-        #include <stdarg.h>
+#       include <stdio.h>
+#       include <stdarg.h>
 
-        #ifdef __linux__
-        #       include <semaphore.h>
-        #elif _WIN32
-        #       include <Windows.h>
-        #       include <synchapi.h>
-        #endif
+#       ifdef __linux__
+#              include <semaphore.h>
+#       elif _WIN32
+#              include <Windows.h>
+#              include <synchapi.h>
+#       endif
 
-        #define fprintf     _tests_fprintf
-        #define snprintf    _tests_snprintf
-        #define vsnprintf   _tests_vsnprintf
-        #define malloc      _tests_malloc
-        #define strlen      _tests_strlen
+#       define fprintf     _tests_fprintf
+#       define snprintf    _tests_snprintf
+#       define vsnprintf   _tests_vsnprintf
+#       define malloc      _tests_malloc
+#       define strlen      _tests_strlen
 
         void *_tests_malloc(size_t size);
         int _tests_fprintf(FILE *stream, const char *format, ...);
@@ -49,17 +49,17 @@
         int _tests_vsnprintf(char *str, size_t size, const char *format, va_list ap);
         size_t _tests_strlen(const char *s);
 
-        #ifdef __linux__
-                #define sem_init    _tests_sem_init
-                #define sem_wait    _tests_sem_wait
+#       ifdef __linux__
+#               define sem_init    _tests_sem_init
+#               define sem_wait    _tests_sem_wait
                 int _tests_sem_init(sem_t *sem, int pshared, unsigned int value);
                 int _tests_sem_wait(sem_t *sem);
-        #elif _WIN32
-                #define CreateMutex _tests_CreateMutex
-                #define WaitForSingleObject _tests_WaitForSingleObject
+#       elif _WIN32
+#               define CreateMutex _tests_CreateMutex
+#               define WaitForSingleObject _tests_WaitForSingleObject
                 HANDLE _tests_CreateMutex(LPSECURITY_ATTRIBUTES lpMutexAttributes, BOOL bInitialOwner, LPCTSTR lpName);
                 DWORD _tests_WaitForSingleObject(HANDLE hHandle, DWORD dwMilliseconds);
-        #endif
+#       endif
 #endif
 
 #include <stdbool.h>
@@ -85,36 +85,35 @@ _YALL_PUBLIC bool yall_is_debug(void);
 
 #ifdef DEBUG
 
-        #include <stdio.h>
+#       include <stdio.h>
+#       include "yall/log.h"
 
-        #include "yall/log.h"
-
-        #define _YALL_DBG_EMERG(...)    \
+#       define _YALL_DBG_EMERG(...)    \
                 if (yall_is_debug()) _YALL_LOG("yall", yall_emerg, ##__VA_ARGS__)
-        #define _YALL_DBG_ALERT(...)    \
+#       define _YALL_DBG_ALERT(...)    \
                 if (yall_is_debug()) _YALL_LOG("yall", yall_alert, ##__VA_ARGS__)
-        #define _YALL_DBG_CRIT(...) \
+#       define _YALL_DBG_CRIT(...) \
                 if (yall_is_debug()) _YALL_LOG("yall", yall_crit, ##__VA_ARGS__)
-        #define _YALL_DBG_ERR(...) \
+#       define _YALL_DBG_ERR(...) \
                 if (yall_is_debug()) _YALL_LOG("yall", yall_err, ##__VA_ARGS__)
-        #define _YALL_DBG_WARNING(...) \
+#       define _YALL_DBG_WARNING(...) \
                 if (yall_is_debug()) _YALL_LOG("yall", yall_warning, ##__VA_ARGS__)
-        #define _YALL_DBG_NOTICE(...) \
+#       define _YALL_DBG_NOTICE(...) \
                 if (yall_is_debug()) _YALL_LOG("yall", yall_notice, ##__VA_ARGS__)
-        #define _YALL_DBG_INFO(...) \
+#       define _YALL_DBG_INFO(...) \
                 if (yall_is_debug()) _YALL_LOG("yall", yall_info, ##__VA_ARGS__)
-        #define _YALL_DBG_DEBUG(...) \
+#       define _YALL_DBG_DEBUG(...) \
                 if (yall_is_debug()) _YALL_LOG("yall", yall_debug, ##__VA_ARGS__)
 #else
 
-        #define _YALL_DBG_EMERG(...)
-        #define _YALL_DBG_ALERT(...)
-        #define _YALL_DBG_CRIT(...)
-        #define _YALL_DBG_ERR(...)
-        #define _YALL_DBG_WARNING(...)
-        #define _YALL_DBG_NOTICE(...)
-        #define _YALL_DBG_INFO(...)
-        #define _YALL_DBG_DEBUG(...)
+#       define _YALL_DBG_EMERG(...)
+#       define _YALL_DBG_ALERT(...)
+#       define _YALL_DBG_CRIT(...)
+#       define _YALL_DBG_ERR(...)
+#       define _YALL_DBG_WARNING(...)
+#       define _YALL_DBG_NOTICE(...)
+#       define _YALL_DBG_INFO(...)
+#       define _YALL_DBG_DEBUG(...)
 
 #endif
 
