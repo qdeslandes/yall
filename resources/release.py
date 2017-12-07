6a485669295239532a7ea36bb6a0383a58215bc8
Naccyde <naccyde@naccyde.eu>
2017-12-07 14:27:19 +0100

    Update release.py

--- resources/release.py
+++ resources/release.py
@@ -16,7 +16,7 @@ def runProcess(cmd):
 	customEnv = os.environ.copy()
 	customEnv["LC_ALL"] = "C"
 	process = subprocess.run(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=customEnv)
-	print(shlex.split(cmd))
+
 	return process.returncode, process.stdout.decode('UTF-8'), process.stderr.decode('UTF-8')
 
 def getMergeBranch():
