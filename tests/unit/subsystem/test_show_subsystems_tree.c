304a9b3b7a22ebf3c4116a63f776c63ec75eee77
naccyde <naccyde@naccyde.eu>
2017-12-09 18:02:22 +0100

    Add show_subsystems_tree() tests
    Signed-off-by: naccyde <naccyde@naccyde.eu>

---
+++ tests/unit/subsystem/test_show_subsystems_tree.c
@@ -0,0 +1,40 @@
+#include "test_subsystem.h"
+#include "helpers/h_subsystem.h"
+#include "yall/test_yall.h"
+
+extern void tests_yall_init_lib(void);
+extern void tests_yall_close_lib(void);
+
+/*
+ * No debug mode
+ */
+Test(subsystem, test_show_subsystems_tree0, .init=tests_yall_init_lib, .fini=tests_yall_close_lib)
+{
+	yall_disable_debug();
+
+	yall_show_subsystems_tree();
+	cr_assert(1);
+
+	/*
+	 * TODO : it would be better, once show_subsystems_tree called to check
+	 * stderr content.
+	 */
+}
+
+/*
+ * Debug mode
+ */
+Test(subsystem, test_show_subsystems_tree1, .init=tests_yall_init_lib, .fini=tests_yall_close_lib)
+{
+	yall_enable_debug();
+
+	yall_show_subsystems_tree();
+	cr_assert(1);
+
+	yall_disable_debug();
+
+	/*
+	 * TODO : it would be better, once show_subsystems_tree called to check
+	 * stderr content.
+	 */
+}
