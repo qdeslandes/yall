b9f84eaa9b10c108ac561547c33de5048b017aca
Quentin Deslandes <naccyde@naccyde.eu>
2017-12-11 16:04:11 +0100

    Feature/create deb rpm packages (#114)
    * Add COMPONENT to install() calls
    * Add DEB & RPM build on Linux
    * Update to 1.3.1

--- resources/CMake/package.cmake
+++ resources/CMake/package.cmake
@@ -2,8 +2,13 @@
 # Redistribution and use of this file is allowed according to the terms of the MIT license.
 # For details see the LICENSE file distributed with yall.
 
-set(CPACK_GENERATOR "ZIP")
+if (UNIX)
+    set(CPACK_GENERATOR "DEB;ZIP;RPM")
+elseif (WIN32)
+    set(CPACK_GENERATOR "ZIP")
+endif ()
 
+SET(CPACK_OUTPUT_FILE_PREFIX packages)
 set(CPACK_PACKAGE_NAME "yall")
 set(CPACK_PACKAGE_VERSION_MAJOR ${YALL_VERSION_MAJOR})
 set(CPACK_PACKAGE_VERSION_MINOR ${YALL_VERSION_MINOR})
