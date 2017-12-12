cf1e6c06415e9ce922fb5efcc71158784dd7c612
naccyde <naccyde@naccyde.eu>
2017-12-12 12:31:56 +0100

    Remove dbg package use dev instead

--- resources/CMake/install.cmake
+++ resources/CMake/install.cmake
@@ -1,17 +1,32 @@
 # Copyright (C) 2017 Quentin "Naccyde" Deslandes.
 # Redistribution and use of this file is allowed according to the terms of the MIT license.
 # For details see the LICENSE file distributed with yall.
-install(FILES README.md DESTINATION share/yall COMPONENT all)
 
-install(FILES changelog DESTINATION share/yall COMPONENT dev)
+install(
+	FILES README.md
+	DESTINATION share/yall
+	CONFIGURATIONS Release)
 
-install(TARGETS yall_static yall_shared
-	RUNTIME DESTINATION bin COMPONENT all
-	ARCHIVE DESTINATION lib COMPONENT all
-	LIBRARY DESTINATION lib COMPONENT all)
+install(
+	FILES changelog
+	DESTINATION share/yall
+	CONFIGURATIONS Release)
 
-install(DIRECTORY ${CMAKE_SOURCE_DIR}/include DESTINATION ./ COMPONENT dev PATTERN "version.h.in" EXCLUDE)
+install(TARGETS yall EXPORT yall_targets
+	RUNTIME DESTINATION bin CONFIGURATIONS Release Debug
+	ARCHIVE DESTINATION lib CONFIGURATIONS Release Debug
+	LIBRARY DESTINATION lib CONFIGURATIONS Release Debug
+	INCLUDES DESTINATION include)
+
+install(
+	DIRECTORY ${CMAKE_SOURCE_DIR}/include
+	DESTINATION ./
+	CONFIGURATIONS Debug
+	PATTERN "version.h.in" EXCLUDE)
 
 if (WIN32)
-	install(FILES external/lib/win32/msvc12/pthreadVC2.dll DESTINATION bin COMPONENT all)
+	install(
+		FILES external/lib/win32/msvc12/pthreadVC2.dll
+		DESTINATION bin
+		CONFIGURATIONS Release)
 endif ()
