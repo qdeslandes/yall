7ab5d2672599969a7f2bca84f2ff06c44614ce19
naccyde <naccyde@naccyde.eu>
2017-12-14 22:36:19 +0100

    Add GCC warnings

--- resources/cmake/yall.cmake
+++ resources/cmake/yall.cmake
@@ -6,10 +6,18 @@
 	Yall objects compilation
 #]]
 if (UNIX)
+	# For more infos about the used flags :
+	#	* https://stackoverflow.com/questions/3375697/useful-gcc-flags-for-c
+	#	* https://kristerw.blogspot.fr/2017/09/useful-gcc-warning-options-not-enabled.html
 	# Compile options
-	set(_PVT_OPT -Wall -Wextra -std=gnu11 -fvisibility=hidden -fPIC)
+	set(_PVT_OPT -Wall -Wextra -Wconversion -ftrapv -Wfloat-equal -Wundef
+		-Wshadow -Wpointer-arith -Wcast-align -Wstrict-prototypes
+		-Wwrite-strings -Waggregate-return -Wuninitialized
+		-Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wrestrict
+		-Wnull-dereference -Wdouble-promotion
+		-fvisibility=hidden -fPIC)
 	set(_PVT_OPT_DEBUG -O0)
-	set(_PVT_OPT_RELEASE -O3)
+	set(_PVT_OPT_RELEASE -O3 -Werror)
 elseif (WIN32)
 	# Compile options
 	set(_PVT_OPT /wd4820 /wd4255 /wd4127 /wd4210 /wd6031 /wd4706 /wd28252 /wd28253 /wd4172 /wd4100 /wd4204 /wd4221 /Wall)
@@ -34,9 +42,9 @@ target_compile_definitions(yall
 	PRIVATE ${_PVT_DEF})
 
 target_include_directories(yall
-	PUBLIC 
+	PUBLIC
 		${_PUB_INCDIR}
-		$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>  
+		$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
 		$<INSTALL_INTERFACE:include>
 	PRIVATE
 		${CMAKE_BINARY_DIR}/generated_headers)
