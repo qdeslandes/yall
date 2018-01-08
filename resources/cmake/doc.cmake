7b65b6b5db906b79f837f819426e53d763b47029
naccyde <naccyde@naccyde.eu>
2018-01-08 22:28:27 +0100

    Generate Doxygen config file

---
+++ resources/cmake/doc.cmake
@@ -0,0 +1,5 @@
+# Copyright (C) 2017 Quentin "Naccyde" Deslandes.
+# Redistribution and use of this file is allowed according to the terms of the MIT license.
+# For details see the LICENSE file distributed with yall.
+
+configure_file(${CMAKE_SOURCE_DIR}/resources/Doxyfile.in ${CMAKE_BINARY_DIR}/doc/Doxyfile)
