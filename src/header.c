7d3d7a5479afde9ae5c2e94663f3953e8c1e5540
naccyde <naccyde@naccyde.eu>
2018-01-10 13:54:03 +0100

    Remove same case in switch statement

--- src/header.c
+++ src/header.c
@@ -122,14 +122,11 @@ static void set_matches_and_header(enum header_type hdr_type,
 	char **header, enum yall_matches **matches)
 {
 	switch (hdr_type) {
-	case std_header:
-		*matches = std_matches;
-		*header = std_header_format;
-		break;
 	case call_header:
 		*matches = call_matches;
 		*header = call_header_format;
 		break;
+	case std_header:
 	default:
 		*matches = std_matches;
 		*header = std_header_format;
