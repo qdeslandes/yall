7c341f954ed5e0e7111e55f413145798ab4df2f6
Naccyde <naccyde@naccyde.eu>
2018-11-30 14:51:15 +0100

    Add call data benchmark

--- tests/benchmark/add_call_data.c
+++ tests/benchmark/add_call_data.c
@@ -5,17 +5,18 @@ void benchmark_add_call_data(uint32_t loops, uint32_t nb_messages)
 	double t_dt_s = 0;
 
 	for (uint32_t i = 0; i < loops; ++i) {
-		yall_call_data d = { 0 };
-		init_call_data(&d);
+		yall_call_data *d = call_data_new();
 
 		clock_t begin = clock();
 
 		for (uint32_t j = 0; j < nb_messages; ++j) {
-			yall_call_add_line(&d, 1, "Testing %d", j);
+			yall_call_add_line(d, 1, "Testing %d", j);
 		}
 
 		clock_t elapsed = clock() - begin;
 		t_dt_s += (double)elapsed / CLOCKS_PER_SEC;
+
+		call_data_delete(d);
 	}
 
 	uint32_t t_msg = nb_messages * loops;
@@ -34,4 +35,8 @@ BENCHMARK(benchmark_add_call_data(20, 10000));
  * 	- Total time: 11.027s
  * 	- Time for 10000 messages (avg.): 0.551s
  * 	- Messages per second (avg.): 18137
+ * 30/11/18 - Use llist instead of a custom linked-list
+ * 	- Total time: 0.070s
+ * 	- Time for 10000 messages (avg.): 0.004s
+ *	- Messages per second (avg.): 2843818
  */
